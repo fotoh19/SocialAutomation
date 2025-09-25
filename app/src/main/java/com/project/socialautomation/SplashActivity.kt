b4f969a515f1b1d2bd09aaf87d8e8fc5c644ff83
fotoh19 <129291991+fotoh19@users.noreply.github.com>
2025-09-25 21:46:51 +0300

    Splash check

--- app/src/main/java/com/project/socialautomation/SplashActivity.kt
+++ app/src/main/java/com/project/socialautomation/SplashActivity.kt
@@ -1,5 +1,6 @@
 package com.project.socialautomation
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
@@ -7,19 +8,27 @@ import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.project.socialautomation.databinding.ActivitySplashBinding
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivitySplashBinding
+    private lateinit var tokenManager: TokenManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        tokenManager = TokenManager(this)
+
         Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, OnBoarding1::class.java)
-            startActivity(intent)
+            if (tokenManager.getTwitterToken() != null) {
+                startActivity(Intent(this, Home::class.java))
+            } else {
+                startActivity(Intent(this, OnBoarding1::class.java))
+            }
             finish()
-        }
-            , 2500)
+        }, 2500)
     }
 }
